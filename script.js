760223244dd90df4fd940fd613a5a2eae18337b2
Abel2705 <abelcruz0305@gmail.com>
2025-10-28 15:27:44 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById('btn');
+const mensaje = document.getElementById('mensaje');
+
+btn.addEventListener('click', () => {
+    mensaje.textContent = '¡Genial! Tu página está funcionando correctamente 🚀';
+});
